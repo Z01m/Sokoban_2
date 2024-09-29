446a7278131b3abf9323bff15d22cfe68af58a19
Z01m <belstep3@gmail.com>
2024-09-29 19:56:05 +0300

    Initial commit

---
+++ Sokoban_2/Map.cs
@@ -0,0 +1,168 @@
+﻿namespace Sokoban_2;
+
+public class Map
+{
+    private static Map instance;
+
+    // Приватный конструктор
+    private Map()
+    {
+    }
+
+    public static Map Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Map();
+            }
+            return instance;
+        }
+    }
+    
+    public static string [] LevelMap =  {
+        "########",
+        "#X.....#",
+        "#@.....#",
+        "#....@.#", 
+        "#......#",
+        "#*...*.#",
+        "#......#",
+        "########"
+    };
+    
+    public List<(int x, int y)> AllBox { get; set; }
+    public List<(int x, int y)> AllPoint { get; set; }
+    
+    public void DrawMap(string [] map)
+    {
+        foreach (var str in map)
+        {
+            Console.WriteLine(str);
+        }
+    }
+    
+    public void ChangeMap(int x, int y, char c) // функция для изменения карты
+    {
+        char[] row = LevelMap[y].ToCharArray();
+        row[x] = c;
+        LevelMap[y] = new string(row);
+    }
+
+    public (int x, int y) GetPlayerPos() //находит игрока на карте и его позицию игрока 
+    {
+        try
+        {
+            for (int i = 1; i < LevelMap.Length; i++)
+            {
+                for (int j = 1; i < LevelMap[i].Length; j++)
+                {
+                    if (LevelMap[i][j] == 'X')
+                    {
+                        return (i,j);
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return (0, 0);
+    }
+
+    public List<(int x, int y)> GetBoxPosition() //находит все ящики на карте и возвращает лист с их координатами 
+    {
+        List<(int x, int y)> res = new List<(int x, int y)>();
+        for (int i = 0; i < LevelMap.Length; i++)
+        {
+            for (int j = 0; j < LevelMap[i].Length; j++)
+            {
+                if (LevelMap[i][j] == '@')
+                {
+                    res.Add((i,j));
+                }
+            }
+        }
+
+        return res;
+    }
+    
+    public List<(int x, int y)> GetPointPosition() //находит все точки на карте и возвращает лист с их координатами 
+    {
+        List<(int x, int y)> res = new List<(int x, int y)>();
+        for (int i = 0; i < LevelMap.Length; i++)
+        {
+            for (int j = 0; j < LevelMap[i].Length; j++)
+            {
+                if (LevelMap[i][j] == '*')
+                {
+                    res.Add((i,j));
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public bool BoxIsFind(int x, int y) //проверяет есть ли ящик по данным координатам
+    {
+        int index = AllBox.FindIndex(box => box.x == y && box.y == x);
+        if (index != -1)
+            return true;
+        return false;
+    }
+    
+    public bool BoxIsFind(int x, int y,List<(int x,int y)> boxes) //проверяет есть ли ящик по данным координатам
+    {
+        int index = boxes.FindIndex(box => box.x == y && box.y == x);
+        if (index != -1)
+            return true;
+        return false;
+    }
+
+    public void ChangeBoxOnMap()
+    {
+        foreach (var vec in AllBox)
+        {
+            ChangeMap((int)vec.y,(int)vec.x,'@');
+        }
+    }
+
+    public void MoveBox((int x,int y) playerPos,(int x,int y) delta)
+    {
+        int index = AllBox.FindIndex(box =>
+            box.x == playerPos.y + delta.y && box.y == playerPos.x + delta.x);
+        if (index != -1)
+        {
+            AllBox[index] = (playerPos.y + 2 * delta.y, playerPos.x + 2 * delta.x);
+        }
+    }
+    
+    public List<(int x,int y)> MoveBox((int x,int y) playerPos,(int x,int y) delta,List<(int x,int y)>boxes)
+    {
+        List<(int x,int y)> tmpBoxes =new(boxes);
+        int index = tmpBoxes.FindIndex(box =>
+            box.x == playerPos.y + delta.y && box.y == playerPos.x + delta.x);
+        if (index != -1)
+        {
+            tmpBoxes[index] = (playerPos.y + 2 * delta.y, playerPos.x + 2 * delta.x);
+        }
+
+        return tmpBoxes;
+    }
+
+    public bool IsWin()
+    {
+        int count = 0;
+        foreach (var point in AllPoint)
+        {
+            if (BoxIsFind(point.y, point.x))
+                count++;
+        }
+        return count == AllBox.Count;
+    }
+}
---
+++ Sokoban_2/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+namespace Sokoban_2
+{
+
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Map.Instance.AllBox = Map.Instance.GetBoxPosition();
+            Map.Instance.AllPoint = Map.Instance.GetPointPosition();
+            bfs bfs = new bfs();
+            bfs._BFS_();
+            /*Player player = new Player();
+           player.PlayerPos = Map.Instance.GetPlayerPos();
+           Map.Instance.AllBox = Map.Instance.GetBoxPosition();
+           Map.Instance.AllPoint = Map.Instance.GetPointPosition();
+           Console.CursorVisible = false;
+           while (true)
+           {
+               if (Map.Instance.IsWin())
+               {
+                   Console.WriteLine("WIn!");
+                   return;
+               }
+
+               Console.Clear();
+               Map.Instance.DrawMap(Map.LevelMap);
+               ConsoleKeyInfo key = Console.ReadKey(true);
+               player.HireHero();
+               if (key.Key == ConsoleKey.W || key.Key == ConsoleKey.UpArrow)
+               {
+                   player.MovePlayer(player.PlayerPos,(0,-1));
+
+               }
+               if (key.Key == ConsoleKey.S || key.Key == ConsoleKey.DownArrow)
+               {
+                   player.MovePlayer(player.PlayerPos,(0,1));
+
+               }
+               if (key.Key == ConsoleKey.A || key.Key == ConsoleKey.LeftArrow)
+               {
+                   player.MovePlayer(player.PlayerPos,(-1,0));
+               }
+               if (key.Key == ConsoleKey.D || key.Key == ConsoleKey.RightArrow)
+               {
+                   player.MovePlayer(player.PlayerPos,(1,0));
+               }
+               player.DrawPlayer();
+           }*/
+        }
+        
+    }
+}
